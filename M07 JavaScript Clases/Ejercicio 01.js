fba5fd523dac1f4546c23981fc05e2d91d2c86de
lennyz0689 <89938536+lennyz0689@users.noreply.github.com>
2023-04-10 13:42:31 -0500

    una parte del modulo clases

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -10,12 +10,27 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
+   class Usuario{
+      constructor(usuario, nombre, email, password){
+         this.usuario = usuario
+         this.nombre = nombre
+         this.email = email
+         this.password = password
+      }
+      saludar(){
+         return 'Hola, mi nombre es '+ this.nombre 
+      }
+      }
+      return Usuario
 }
 
 function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
+   Usuario.prototype.saludar=function(){
+      return "Hello World!"
+   }
 }
 
 function agregarStringInvertida() {
